4c6e99eca557f75471d5cf5c5dddb4c51f54ef08
Gingikinz <spencerjholt@outlook.com>
2018-11-29 11:09:20 -0700

    Functons and Buttons

--- star.js
+++ star.js
@@ -4,13 +4,14 @@ console.log(starships)
 //console.log(starships[0].name);
 
 const shipContainer = document.querySelector('#container')
-//const statsContainer = document.querySelector('#scontainer')
 
-starships.forEach(starship => {
-let card = document.createElement('div')
+function cardMaker(x) {
+
+x.forEach(starship => {
+let card = document.createElement('figure')
 card.className = 'card'
-card.addEventListener( 'click', () =>  {
-    card.classList.toggle('flipped');})
+    card.addEventListener( 'click', () =>  {
+        card.classList.toggle('flipped');})
 let fig = document.createElement('figure')
 fig.className = 'card__face card__face--front'
 let cap = document.createElement('figcaption')
@@ -19,21 +20,22 @@ img.src = `/assets/simg/${starship.name}.png`
 cap.textContent = starship.name
 fig.appendChild(img)
 fig.appendChild(cap)
-
 card.appendChild(fig)
-
 let back = document.createElement('figure')
 back.className = 'card__face card__face--back'
+// let picture = document.createElement('img')
 let shipName = document.createElement('h2')
 let shipModel = document.createElement('p')
 let shipClass = document.createElement('p')
 let shipCrew = document.createElement('p')
 let shipDrive = document.createElement('p')
+//picture.src = `/assets/simg/SWr.png`
 shipName.textContent = starship.name
 shipModel.textContent = `Model: ${starship.model}`
 shipClass.textContent = `Class: ${starship.starship_class}`
 shipCrew.textContent = `Crew: ${starship.crew}`
 shipDrive.textContent = `Hyperdrive rating: ${starship.hyperdrive_rating}`
+//back.appendChild(picture)
 back.appendChild(shipName)
 back.appendChild(shipModel)
 back.appendChild(shipClass)
@@ -43,47 +45,57 @@ back.appendChild(shipDrive)
 card.appendChild(back)
 shipContainer.appendChild(card)
 
+});
+};
+
+cardMaker(starships)
+
+function spaceship(name, model, ship_class, crew, hdr){
+    this.name = name
+    this.model = model
+    this.class = ship_class
+    this.crew = crew
+    this.hdr = hdr
+}
+
+let enterprise = new spaceship("Enterprise", "Constitution", "Cruiser", "250", "<10")
+
+console.log(enterprise)
 
 
-    
- 
+// function Trek() {
+//     cardMaker(enterprise)
+// };
+
+const feds = [
+    {
+    "name": "Enterprise",
+    "model": "Constitution",
+    "class": "Cruiser",
+    "crew": "~430",
+    "hdr": "<10"
+}];
+
+let bon = document.createElement("button");
+bon.innerHTML = "Another Spaceship?";
+
+var location = document.getElementById("newCardButton")
+location.appendChild(bon)
+
+bon.addEventListener('click', () => {
+    alert("Sir, it's the Enterprise!")
+    cardMaker(feds)
 });
 
 
-//     starships.forEach(starship => {
-//     let back = document.createElement('figure')
-//     let shipName = document.createElement('h2')
-//     let shipModel = document.createElement('p')
-//     let shipClass = document.createElement('p')
-//     let shipCrew = document.createElement('p')
-//     let shipDrive = document.createElement('p')
-//     shipName.textContent = starship.name
-//     shipModel.textContent = `Model: ${starship.model}`
-//     shipClass.textContent = `Class: ${starship.starship_class}`
-//     shipCrew.textContent = `Crew: ${starship.crew}`
-//     shipDrive.textContent = `Hyperdrive rating: ${starship.hyperdrive_rating}`
-//     back.appendChild(shipName)
-//     back.appendChild(shipModel)
-//     back.appendChild(shipClass)
-//     back.appendChild(shipCrew)
-//     back.appendChild(shipDrive)
-    
-//     //statsContainer.appendChild(back)
 
-//     shipContainer.appendChild(back) /*Works*/
-// //shipContainer.appendChild(statsContainer)
-// //console.log(statsContainer)
-// });
+/* example construtor function: */
+// function Bird() {
+//     this.name = "derp";
+//     this.color = "red";
+//     this.can_fly = false;
+// };
 
-//console.log(starships.name)
+// let derp = new Bird()
 
 
-    // let statsList = document.createElement('backList')
-    // // console.log(stats)
-    // statsList.appendChild(stats)
-
- 
-// var flip = document.querySelector('figure');
-// flip.addEventListener( 'click', function() {
-//     flip.classList.toggle('flipped');
-// });
